e8f1144f0f4f2ad698e9a13a3fdd59f93a2c6409
Ayush Deshpande <93706892+asyncdoggo@users.noreply.github.com>
2023-10-10 10:44:55 +0000

    Create react app

---
+++ src/pages/home.jsx
@@ -0,0 +1,26 @@
+export default function Home() {
+    return (
+        <>
+            <section id="hero">
+            <div class="hero-container">
+            <div class="hero-content">
+                <h1 class="header-font-800">Microsoft Learn Student Club MMCOE</h1>
+                <p class="body-font-500">Be A Force For Good.</p>
+            </div>
+            <div class="hero-img">
+                <img src="assets/logos/logo_main.png" alt="" id="hero-img" />
+            </div>
+            </div>
+            <div class="icons">
+            <img src="assets/logos/teams.png" alt="" id="icon" />
+            <img src="assets/logos/office.png" alt="" id="icon" />
+            <img src="assets/logos/excel.png" alt="" id="icon" />
+            <img src="assets/logos/devops.png" alt="" id="icon" />
+            <img src="assets/logos/github.png" alt="" id="icon" />
+            <img src="assets/logos/azure.png" alt="" id="icon" />
+            <img src="assets/logos/linkedin.png" alt="" id="icon" />
+            </div>
+        </section>
+      </>
+    )
+}
